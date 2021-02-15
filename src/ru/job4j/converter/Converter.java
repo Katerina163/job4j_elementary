f3e5b8ae6135df04a8ea375f560b0fed9044abfd
Nefedova <Katerina163>
2021-02-15 16:23:09 +0400

    7. Расстояние между точками в системе координат [#475130]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,12 +2,12 @@ package ru.job4j.converter;
 
 public class Converter {
     public static int rubleToEuro(int value) {
-        int rsl = value / 70;/* формула перевода рублей в евро. */
-        return rsl;
+        /* формула перевода рублей в евро. */
+        return value / 70;
     }
     public static int rubleToDollar(int value) {
-        int rsl = value / 60; /* формула перевода рублей в доллары. */
-        return rsl;
+        /* формула перевода рублей в доллары. */
+        return value / 60;
     }
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
