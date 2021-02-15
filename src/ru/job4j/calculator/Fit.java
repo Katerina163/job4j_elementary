d2f33e97d03a97c2b43ea947cdb9d6671c8dc847
Nefedova <Katerina163>
2021-02-15 16:46:33 +0400

    6. Идеальный вес [#475130]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,10 +2,10 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        return (height-100)*1.15;
+        return (height-100) *1.15;
     }
     public static double womanWeight(short height) {
-        return (height-110)*1.15;
+        return (height-110) *1.15;
     }
     public static void main(String[] args) {
         short height = 187; //рост в см
